0097e5043834898cb9944cf24679d08838cf500b
Odelia <deelyohuang@gmail.com>
2022-12-06 19:14:11 +0100

    init react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <head>  
+      <title> 
+          Welcome to my page, world.</title>  
+      </head>  
+
+      <h3>What inspires me?</h3>
+      <body>  
+      <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+      <a data-pin-do="embedBoard" data-pin-board-width="600" data-pin-scale-height="480" data-pin-scale-width="600" href="https://www.pinterest.com/deelyohuang/visual-literacy-log/"></a>
+      </body>  
+    </div>
+  );
+}
+
+export default App;
