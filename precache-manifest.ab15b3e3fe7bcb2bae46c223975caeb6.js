44ba331ad2e71610c9bf158c30cba9391c38a095
Odelia <deelyohuang@gmail.com>
2023-01-11 23:22:58 -0500

    Updates

---
+++ precache-manifest.ab15b3e3fe7bcb2bae46c223975caeb6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "080b4618579d120af324",
+    "url": "/static/css/main.c67c5ed1.chunk.css"
+  },
+  {
+    "revision": "080b4618579d120af324",
+    "url": "/static/js/main.da4d65ca.chunk.js"
+  },
+  {
+    "revision": "ff49df8bb83d13228e07",
+    "url": "/static/js/runtime~main.fb0988b1.js"
+  },
+  {
+    "revision": "3008e5821c6aaefd265c",
+    "url": "/static/js/2.a28d2e57.chunk.js"
+  },
+  {
+    "revision": "36bde6f0914f99b07518",
+    "url": "/static/js/3.7cca2875.chunk.js"
+  },
+  {
+    "revision": "57c7385faedbde3f6dfc76e08621b9ad",
+    "url": "/static/media/logo.57c7385f.jpg"
+  },
+  {
+    "revision": "a5d9bba1f92a6706ae350a7f5704355f",
+    "url": "/static/media/teph1.a5d9bba1.jpg"
+  },
+  {
+    "revision": "2e10c808bc163e0e28765f30e48fbe7d",
+    "url": "/static/media/teph2.2e10c808.jpg"
+  },
+  {
+    "revision": "8ac690ec661ea90e685d08da2511b926",
+    "url": "/static/media/teph3.8ac690ec.jpg"
+  },
+  {
+    "revision": "8101970a73348a1971536dcca9745891",
+    "url": "/static/media/teph4.8101970a.jpg"
+  },
+  {
+    "revision": "8c77e703881ea72268aa534a6df19ae7",
+    "url": "/index.html"
+  }
+];
