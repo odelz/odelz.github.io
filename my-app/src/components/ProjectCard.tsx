fc367d39682309c4fb73f3a452c8ffb844e49653
Odelia <deelyohuang@gmail.com>
2023-01-11 23:26:11 -0500

    more updates

--- my-app/src/components/ProjectCard.tsx
+++ my-app/src/components/ProjectCard.tsx
@@ -10,7 +10,6 @@ import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -71,9 +70,6 @@ const ProjectCard: FC<ProjectCardProps> = ({ title, logo, img, description, date
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
