4688ee17a511d005216eadc4bbf264670f33184b
Odelia <deelyohuang@gmail.com>
2023-07-24 21:57:44 +0200

    me books

--- my-app-nextjs/app/bookshelf/page.tsx
+++ my-app-nextjs/app/bookshelf/page.tsx
@@ -2,16 +2,34 @@
 import { ref, set, push, onValue } from "firebase/database";
 import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
+import Image from "next/image";
 
 interface Book {
   id: string;
   title: string;
   author: string;
   review: number;
+  imageUrl: string;
 }
 
 const MyBooksPage = () => {
-  const [books, setBooks] = useState<Book[]>([]);
+  const [books, setBooks] = useState<Book[]>([
+    {
+      id: '1',
+      title: 'Middlegame',
+      author: 'Seanan McGuire',
+      review: 4,
+      imageUrl: '/images/books/middlegame.jpg',
+    },
+    {
+      id: '2',
+      title: 'Every Heart a Doorway',
+      author: 'Seanan McGuire',
+      review: 5,
+      imageUrl: '/images/books/everyheart.jpg',
+    },
+    // Add more books here as needed
+  ]);
 
 
   const reference = ref(db, "books/");
@@ -45,6 +63,9 @@ const MyBooksPage = () => {
             <strong>Author:</strong> {book.author}
             <br />
             <strong>Review:</strong> {book.review ? book.review : 'No review yet'}
+            <br />
+            <Image src={book.imageUrl} alt={book.title} width={200} height={300} />
+            {/* Set the appropriate width and height values based on your image dimensions */}
             <hr />
           </li>
         ))}
