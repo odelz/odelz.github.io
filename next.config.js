8691c515039ec84f448d087ec659c91dfc5a2f66
Odelia <deelyohuang@gmail.com>
2023-07-24 22:43:11 +0200

    ??

--- next.config.js
+++ next.config.js
@@ -6,15 +6,6 @@
 const nextConfig = {
   output: "export",
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
