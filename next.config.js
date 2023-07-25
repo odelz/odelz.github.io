fc888f02f6ddab68d4691b07b61fa01da2f57188
Odelia <deelyohuang@gmail.com>
2023-07-25 14:53:06 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
-  output: "export",
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
