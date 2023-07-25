a4a00afca7289ced3b6ab2615cc7e9db1e61e9f9
Odelia <deelyohuang@gmail.com>
2023-07-25 15:50:21 +0200

    another one

--- next.config.js
+++ next.config.js
@@ -4,9 +4,15 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  images: {},
-  assetPrefix: '/',
-
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "": undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
