3edb528bd9b895a99e16739b1bf7c48309aad815
Odelia <deelyohuang@gmail.com>
2023-07-25 14:51:18 +0200

    config??

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-// @ts-check
 
-/**
- * @type {import('next').NextConfig}
- **/
 const nextConfig = {
   output: "export",
-  experimental: {
-    turbotrace: {
-      // control if the log of turbotrace should contain the details of the analysis, default is `false`
-      logDetail: true,
-      // show all log messages without limit
-      // turbotrace only show 1 log message for each categories by default
-      logAll: true
-    },
-  },
-
 };
 
 module.exports = nextConfig;
